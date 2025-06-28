207f2276474f50260ae6a697b220d8a2ca0f6655
Jonathan Nilsson <lord.jonil@gmail.com>
2025-06-28 16:03:55 +0200

    Pass args

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::{
-    ffi::{c_int, c_void},
-    ptr,
+    env,
+    ffi::{CString, c_int, c_void},
+    iter,
 };
 
 #[link(name = "bgfx_shaderc")]
@@ -9,5 +10,15 @@ unsafe extern "C" {
 }
 
 fn main() {
-    unsafe { cpp_main(0, ptr::null()) };
+    let prog = env::current_exe().unwrap();
+    let args = iter::once(prog.to_string_lossy().into_owned())
+        .chain(env::args())
+        .map(|s| CString::new(s).unwrap())
+        .collect::<Vec<_>>();
+    let args = args
+        .iter()
+        .map(|s| s.as_c_str().as_ptr())
+        .collect::<Vec<_>>();
+
+    unsafe { cpp_main(args.len() as _, args.as_ptr() as _) };
 }
