dc9c0a3ed08d3ffa221cbf5996198120eac66c8e
Jonathan Nilsson <lord.jonil@gmail.com>
2025-06-28 16:20:30 +0200

    Return value

--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,10 @@ use std::{
 
 #[link(name = "bgfx_shaderc")]
 unsafe extern "C" {
-    fn cpp_main(argc: c_int, argv: *const *const c_void);
+    fn cpp_main(argc: c_int, argv: *const *const c_void) -> i32;
 }
 
-pub fn shaderc(args: &[&str]) {
+pub fn shaderc(args: &[&str]) -> i32 {
     let prog = env::current_exe().unwrap();
     let args = iter::once(prog.to_string_lossy().into_owned())
         .chain(args.iter().map(|s| s.to_string()))
@@ -20,5 +20,5 @@ pub fn shaderc(args: &[&str]) {
         .map(|s| s.as_c_str().as_ptr())
         .collect::<Vec<_>>();
 
-    unsafe { cpp_main(args.len() as _, args.as_ptr() as _) };
+    unsafe { cpp_main(args.len() as _, args.as_ptr() as _) }
 }
